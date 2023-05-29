fd55de2d78c0db5b532d4f0b2fe03edd5736111c
Stephan Mueller <smueller@chronox.de>
2023-05-29 11:20:25 +0200

    adjust code
    Signed-off-by: Stephan Mueller <smueller@chronox.de>

--- sym/src/asm/AESNI_x86_64/aes_aesni_x86_64.h
+++ sym/src/asm/AESNI_x86_64/aes_aesni_x86_64.h
@@ -45,22 +45,21 @@ int aesni_set_encrypt_key(const uint8_t *Key, unsigned int bits,
 int aesni_set_decrypt_key(const uint8_t *Key, unsigned int bits,
 			  struct aes_aesni_block_ctx *aes_ctx);
 
-void aesni_ecb_encrypt(const uint8_t *in, uint8_t *out, size_t length,
-		       const struct aes_aesni_block_ctx *aes_ctx, int enc);
-
-void aesni_cbc_encrypt(const uint8_t *in, uint8_t *out, size_t length,
-		       const struct aes_aesni_block_ctx *aes_ctx, uint8_t *iv,
-		       int enc);
-
 void aesni_ctr32_encrypt_blocks(const uint8_t *in, uint8_t *out, size_t blocks,
 				const struct aes_aesni_block_ctx *aes_ctx,
 				const uint8_t *iv);
 
+void aesni_ecb_encrypt(const uint8_t *in, uint8_t *out, size_t length,
+		       const struct aes_aesni_block_ctx *aes_ctx, int enc);
+void aesni_cbc_encrypt(const uint8_t *in, uint8_t *out, size_t length,
+		       const struct aes_aesni_block_ctx *aes_ctx, uint8_t *iv,
+		       int enc);
+
+#if 0
 void aesni_xts_encrypt(const uint8_t *pt, uint8_t *ct, size_t length,
 		       const struct aes_aesni_block_ctx *key1,
 		       const struct aes_aesni_block_ctx *key2,
 		       const uint8_t iv[16]);
-
 void aesni_xts_decrypt(const uint8_t *ct, uint8_t *pt, size_t length,
 		       const struct aes_aesni_block_ctx *key1,
 		       const struct aes_aesni_block_ctx *key2,
@@ -69,12 +68,10 @@ void aesni_xts_decrypt(const uint8_t *ct, uint8_t *pt, size_t length,
 void aesni_ccm64_encrypt_blocks(const uint8_t *pt, uint8_t *ct, size_t blocks,
 				const void *key, const uint8_t iv[16],
 				uint8_t cmac[16]);
-
 void aesni_ccm64_decrypt_blocks(const uint8_t *ct, uint8_t *pt, size_t blocks,
 				const void *key, const uint8_t iv[16],
 				uint8_t cmac[16]);
 
-#if 0
 void aesni_ocb_encrypt(const uint8_t *pt,uint8_t *ct,
 		       size_t blocks, const void *key,
 		       size_t start_block_num,
