fd5e36fcb922c7c09cda2dfc992285d7f5f96d89
Stephan Mueller <smueller@chronox.de>
2023-08-12 22:45:57 +0200

    add ESDM seed source
    Signed-off-by: Stephan Mueller <smueller@chronox.de>

---
+++ drng/src/seeded_rng_esdm.c
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2023, Stephan Mueller <smueller@chronox.de>
+ *
+ * License: see LICENSE file in root directory
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, ALL OF
+ * WHICH ARE HEREBY DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+ * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF NOT ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
+#include <esdm/esdm_rpc_client.h>
+#include <errno.h>
+
+#include "seeded_rng.h"
+
+ssize_t get_full_entropy(uint8_t *buffer, size_t bufferlen)
+{
+	ssize_t ret;
+
+	esdm_invoke(esdm_rpcc_get_random_bytes_full(buffer, bufferlen));
+
+	return ret ? ret : (ssize_t)bufferlen;
+}
