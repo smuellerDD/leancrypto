a214236b17a5665b55554c82e1f3404c84c5ad7e
Stephan Mueller <smueller@chronox.de>
2023-10-03 20:29:15 +0200

    Hash DRBG: remove stale checks to aovid compiler warnings
    Signed-off-by: Stephan Mueller <smueller@chronox.de>

--- drng/src/hash_drbg.c
+++ drng/src/hash_drbg.c
@@ -188,13 +188,6 @@ static void drbg_hash_df(struct lc_drbg_hash_state *drbg, uint8_t *outval,
 		size_t blocklen =
 			min_size(LC_DRBG_HASH_BLOCKLEN, (outlen - len));
 
-		/* shut up -Wstringop-overread */
-		if (len + blocklen > outlen)
-			break;
-
-		/* shut up -Wstringop-overread */
-		//memset(tmp, 0, LC_DRBG_HASH_BLOCKLEN);
-
 		/* 10.3.1 step 4.1 */
 		drbg_hash(drbg, tmp, &data);
 		/* 10.3.1 step 4.2 */
