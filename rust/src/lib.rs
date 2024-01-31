7e568ca1dc38c8f2733c50438b4f68fd1532fe91
Stephan Mueller <smueller@chronox.de>
2024-01-31 23:36:41 +0100

    rust: add application example
    Signed-off-by: Stephan Mueller <smueller@chronox.de>

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,8 +1,99 @@
+/*
+ * Copyright (C) 2024, Stephan Mueller <smueller@chronox.de>
+ *
+ * License: see LICENSE file in root directory
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, ALL OF
+ * WHICH ARE HEREBY DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+ * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF NOT ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+pub fn lcr_sha3_512(msg: &[u8]) -> [u8; 64]
+{
+	let mut digest: [u8; 64] = [0; 64];
+
+	unsafe {
+		lc_hash(lc_sha3_512, msg.as_ptr(), msg.len(),
+			digest.as_mut_ptr());
+	}
+
+	return digest;
+}
+
+pub fn lcr_sha3_384(msg: &[u8]) -> [u8; 48]
+{
+	let mut digest: [u8; 48] = [0; 48];
+
+	unsafe {
+		lc_hash(lc_sha3_384, msg.as_ptr(), msg.len(),
+			digest.as_mut_ptr());
+	}
+
+	return digest;
+}
+
+pub fn lcr_sha3_256(msg: &[u8]) -> [u8; 32]
+{
+	let mut digest: [u8; 32] = [0; 32];
+
+	unsafe {
+		lc_hash(lc_sha3_256, msg.as_ptr(), msg.len(),
+			digest.as_mut_ptr());
+	}
+
+	return digest;
+}
+
+pub fn lcr_sha3_224(msg: &[u8]) -> [u8; 28]
+{
+	let mut digest: [u8; 28] = [0; 28];
+
+	unsafe {
+		lc_hash(lc_sha3_224, msg.as_ptr(), msg.len(),
+			digest.as_mut_ptr());
+	}
+
+	return digest;
+}
+
+pub fn lcr_sha256(msg: &[u8]) -> [u8; 32]
+{
+	let mut digest: [u8; 32] = [0; 32];
+
+	unsafe {
+		lc_hash(lc_sha256, msg.as_ptr(), msg.len(),
+			digest.as_mut_ptr());
+	}
+
+	return digest;
+}
+
+pub fn lcr_sha512(msg: &[u8]) -> [u8; 64]
+{
+	let mut digest: [u8; 64] = [0; 64];
+
+	unsafe {
+		lc_hash(lc_sha512, msg.as_ptr(), msg.len(),
+			digest.as_mut_ptr());
+	}
+
+	return digest;
+}
 
 #[cfg(test)]
 mod tests {
