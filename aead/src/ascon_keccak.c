c4da21313504ceceaa5d7d4505542019ff60e17f
Stephan Mueller <smueller@chronox.de>
2024-03-27 00:34:25 +0100

    fix for in-place decryption
    Signed-off-by: Stephan Mueller <smueller@chronox.de>

--- aead/src/ascon_keccak.c
+++ aead/src/ascon_keccak.c
@@ -17,15 +17,16 @@
  * DAMAGE.
  */
 
-#include "bitshift.h"
-#include "conv_be_le.h"
 #include "alignment.h"
+#include "bitshift.h"
+#include "compare.h"
+#include "ext_headers.h"
 #include "lc_ascon_keccak.h"
 #include "lc_memcmp_secure.h"
 #include "lc_memory_support.h"
 #include "math_helper.h"
 #include "visibility.h"
-#include "xor256.h"
+#include "xor.h"
 
 /*
  * Algorithms with 256 bit security strength based on the fact that the
@@ -41,6 +42,65 @@
  */
 #define LC_AEAD_AK_SHA3_512_INIT 0x0100024000180018
 #define LC_AEAD_AK_SHA3_256_INIT 0x0100044000180018
+
+static void lc_ak_selftest(int *tested, const char *impl)
+{
+	static const uint8_t in[] = {
+		0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09,
+		0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13,
+		0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d,
+		0x1e, 0x1f, 0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27,
+		0x28, 0x29, 0x2a, 0x2b, 0x2c, 0x2d, 0x2e, 0x2f, 0x30, 0x31,
+		0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3a, 0x3b,
+		0x3c, 0x3d, 0x3e, 0x3f,
+	};
+	static const uint8_t key[] = {
+		0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89,
+		0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f, 0x90, 0x91, 0x92, 0x93,
+		0x94, 0x95, 0x96, 0x97, 0x98, 0x99, 0x9a, 0x9b, 0x9c, 0x9d,
+		0x9e, 0x9f,
+	};
+	static const uint8_t iv[] = {
+		0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29,
+		0x2a, 0x2b, 0x2c, 0x2d, 0x2e, 0x2f,
+	};
+	static const uint8_t exp_ct[] = {
+		0x66, 0xeb, 0x20, 0xf0, 0x33, 0xf0, 0x12, 0xe0, 0x5a, 0xe3,
+		0x41, 0x0f, 0xf0, 0xf4, 0xe2, 0x86, 0xa7, 0xec, 0xd6, 0x5a,
+		0x91, 0x74, 0x62, 0x04, 0xcd, 0x4c, 0xb7, 0xf5, 0x65, 0x68,
+		0x98, 0x03, 0x1a, 0xf2, 0x49, 0xcf, 0x9f, 0x09, 0x86, 0x0c,
+		0xe4, 0xda, 0xaa, 0xfa, 0xf0, 0x74, 0xc5, 0x4c, 0x87, 0x66,
+		0x49, 0x53, 0x45, 0x1a, 0x63, 0x3f, 0x57, 0xfa, 0x5c, 0x7b,
+		0x4b, 0xbe, 0xa1, 0xca
+	};
+	static const uint8_t exp_tag[] = {
+		0x03, 0x3b, 0x2d, 0x50, 0x90, 0x16, 0x72, 0x4f, 0x36, 0xf4,
+		0xe3, 0x0a, 0xfd, 0x58, 0x5c, 0x65
+	};
+	uint8_t act_ct[sizeof(exp_ct)] __align(sizeof(uint32_t));
+	uint8_t act_tag[sizeof(exp_tag)] __align(sizeof(uint32_t));
+	char status[35];
+
+	LC_SELFTEST_RUN(tested);
+
+	LC_AK_CTX_ON_STACK(ak, lc_sha3_256);
+
+	assert(!lc_aead_setkey(ak, key, sizeof(key), iv, sizeof(iv)));
+	lc_aead_encrypt(ak, in, act_ct, sizeof(in), in, sizeof(in), act_tag,
+			sizeof(act_tag));
+	snprintf(status, sizeof(status), "%s encrypt", impl);
+	lc_compare_selftest(act_ct, exp_ct, sizeof(exp_ct), status);
+	lc_compare_selftest(act_tag, exp_tag, sizeof(exp_tag), status);
+	lc_aead_zero(ak);
+
+	assert(!lc_aead_setkey(ak, key, sizeof(key), iv, sizeof(iv)));
+	assert(!lc_aead_decrypt(ak, act_ct, act_ct, sizeof(act_ct), in,
+				sizeof(in), act_tag, sizeof(act_tag)));
+
+	snprintf(status, sizeof(status), "%s decrypt", impl);
+	lc_compare_selftest(act_ct, in, sizeof(in), status);
+	lc_aead_zero(ak);
+}
 
 /**
  * @brief Set the key for the encyption or decryption operation
@@ -60,9 +120,9 @@ static int lc_ak_setkey(void *state, const uint8_t *key, size_t keylen,
 	struct lc_ak_cryptor *ak = state;
 	const struct lc_hash *hash = ak->hash;
 	unsigned int i;
-//	static int tested = 0;
+	static int tested = 0;
 
-//	lc_ak_selftest(&tested, "cSHAKE AEAD");
+	lc_ak_selftest(&tested, "Asacon Keccak AEAD");
 
 	memset(state, 0, LC_SHA3_STATE_SIZE);
 
@@ -222,10 +282,17 @@ static int lc_ak_decrypt(void *state, const uint8_t *ciphertext,
 {
 	struct lc_ak_cryptor *ak = state;
 	const struct lc_hash *hash = ak->hash;
-	uint8_t calctag[128] __align(sizeof(uint64_t));
-	uint8_t *calctag_p = calctag;
+	uint8_t calctag[16] __align(sizeof(uint64_t));
+	uint8_t tmp_pt[136] __align(sizeof(uint64_t)) = { 0 };
+	uint8_t *calctag_p = calctag, *pt_p = plaintext;
 	size_t todo;
-	int ret;
+	int ret, zero_tmp = 0;
+
+	/* If we have an in-place cipher operation, we need a tmp-buffer */
+	if (plaintext == ciphertext) {
+		pt_p = tmp_pt;
+		zero_tmp = 1;
+	}
 
 	if (taglen > sizeof(calctag)) {
 		ret = lc_alloc_aligned((void **)&calctag_p,
@@ -240,7 +307,7 @@ static int lc_ak_decrypt(void *state, const uint8_t *ciphertext,
 	/* Plaintext - Insert into rate */
 	while (datalen) {
 		todo = min_size(datalen, hash->rate);
-		lc_keccak_extract_bytes(hash, ak->keccak_state, plaintext, 0,
+		lc_keccak_extract_bytes(hash, ak->keccak_state, pt_p, 0,
 					(unsigned int)todo);
 
 		datalen -= todo;
@@ -258,15 +325,27 @@ static int lc_ak_decrypt(void *state, const uint8_t *ciphertext,
 			 * Perform XOR operation here to ensure decryption in
 			 * place.
 			 */
-			xor_256(plaintext, ciphertext, todo);
+			if (!zero_tmp) {
+				xor_64(pt_p, ciphertext, todo);
+				pt_p += todo;
+			} else {
+				xor_64_3(plaintext, pt_p, ciphertext, todo);
+				plaintext += todo;
+			}
 
-			plaintext += todo;
 			ciphertext += todo;
 		} else {
-			xor_256(plaintext, ciphertext, todo);
-
-			lc_keccak_add_bytes(hash, ak->keccak_state, plaintext,
-					    0, (unsigned int)todo);
+			if (!zero_tmp) {
+				xor_64(pt_p, ciphertext, todo);
+				lc_keccak_add_bytes(hash, ak->keccak_state,
+						    pt_p, 0,
+						    (unsigned int)todo);
+			} else {
+				xor_64_3(plaintext, pt_p, ciphertext, todo);
+				lc_keccak_add_bytes(hash, ak->keccak_state,
+						    plaintext, 0,
+						    (unsigned int)todo);
+			}
 
 			lc_ak_add_padbyte(ak, todo);
 		}
@@ -279,6 +358,9 @@ static int lc_ak_decrypt(void *state, const uint8_t *ciphertext,
 	lc_memset_secure(calctag_p, 0, taglen);
 	if (taglen > sizeof(calctag))
 		lc_free(calctag_p);
+
+	if (zero_tmp)
+		lc_memset_secure(tmp_pt, 0, sizeof(tmp_pt));
 
 	return ret;
 }
