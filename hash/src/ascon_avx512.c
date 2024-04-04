52800cdc0c8ebb95a2eb736bca0f9306817b13e1
Stephan Mueller <smueller@chronox.de>
2024-04-04 21:38:26 +0200

    Ascon: add AVX512 permutation
    The implementation is available as lc_ascon_avx512. Yet, the impl is
    disabled as it is slower than the C implementation.
    
    Signed-off-by: Stephan Mueller <smueller@chronox.de>

---
+++ hash/src/ascon_avx512.c
@@ -0,0 +1,160 @@
+/*
+ * Copyright (C) 2024, Stephan Mueller <smueller@chronox.de>
+ *
+ * License: see LICENSE file in root directory
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, ALL OF
+ * WHICH ARE HEREBY DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+ * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF NOT ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+
+#include "asm/ascon_avx512/round.h"
+
+#include "ascon_avx512.h"
+#include "ascon_hash.h"
+#include "ascon_hash_common.h"
+#include "lc_ascon_hash.h"
+#include "visibility.h"
+
+static inline void ascon_permutation_6(uint64_t s[LC_ASCON_HASH_STATE_WORDS + 3])
+{
+	__m512i *z = (void *)s;
+
+	ascon_permutation_one_avx512(z, 0x96);
+	ascon_permutation_one_avx512(z, 0x87);
+	ascon_permutation_one_avx512(z, 0x78);
+	ascon_permutation_one_avx512(z, 0x69);
+	ascon_permutation_one_avx512(z, 0x5a);
+	ascon_permutation_one_avx512(z, 0x4b);
+}
+
+static inline void ascon_permutation_8(uint64_t s[LC_ASCON_HASH_STATE_WORDS + 3])
+{
+	__m512i *z = (void *)s;
+
+	ascon_permutation_one_avx512(z, 0xb4);
+	ascon_permutation_one_avx512(z, 0xa5);
+	ascon_permutation_one_avx512(z, 0x96);
+	ascon_permutation_one_avx512(z, 0x87);
+	ascon_permutation_one_avx512(z, 0x78);
+	ascon_permutation_one_avx512(z, 0x69);
+	ascon_permutation_one_avx512(z, 0x5a);
+	ascon_permutation_one_avx512(z, 0x4b);
+}
+
+static inline void ascon_permutation_12(uint64_t s[LC_ASCON_HASH_STATE_WORDS + 3])
+{
+	__m512i *z = (void *)s;
+
+	ascon_permutation_one_avx512(z, 0xf0);
+	ascon_permutation_one_avx512(z, 0xe1);
+	ascon_permutation_one_avx512(z, 0xd2);
+	ascon_permutation_one_avx512(z, 0xc3);
+	ascon_permutation_one_avx512(z, 0xb4);
+	ascon_permutation_one_avx512(z, 0xa5);
+	ascon_permutation_one_avx512(z, 0x96);
+	ascon_permutation_one_avx512(z, 0x87);
+	ascon_permutation_one_avx512(z, 0x78);
+	ascon_permutation_one_avx512(z, 0x69);
+	ascon_permutation_one_avx512(z, 0x5a);
+	ascon_permutation_one_avx512(z, 0x4b);
+}
+
+static void ascon_avx512_permutation(void *state, unsigned int rounds)
+{
+	switch (rounds) {
+	case 12:
+		ascon_permutation_12((uint64_t *)state);
+		break;
+	case 8:
+		ascon_permutation_8((uint64_t *)state);
+		break;
+	case 6:
+		ascon_permutation_6((uint64_t *)state);
+		break;
+	default:
+		break;
+	}
+}
+
+static void ascon_absorb(void *state, const uint8_t *in, size_t inlen)
+{
+	ascon_absorb_common(state, in, inlen, ascon_avx512_permutation);
+}
+
+static void ascon_squeeze(void *state, uint8_t *digest)
+{
+	ascon_squeeze_common(state, digest, ascon_permutation_12,
+			     ascon_avx512_permutation);
+}
+
+static const struct lc_hash _ascon_128_avx512 = {
+	.init = ascon_128_init,
+	.update = ascon_absorb,
+	.final = ascon_squeeze,
+	.set_digestsize = NULL,
+	.get_digestsize = ascon_digestsize,
+	.sponge_permutation = ascon_avx512_permutation,
+	.sponge_add_bytes = ascon_c_add_bytes,
+	.sponge_extract_bytes = ascon_c_extract_bytes,
+	.sponge_newstate = ascon_c_newstate,
+	.sponge_rate = 64 / 8,
+	.statesize = sizeof(struct lc_ascon_hash),
+};
+LC_INTERFACE_SYMBOL(const struct lc_hash *, lc_ascon_128_avx512) = &_ascon_128_avx512;
+
+
+static const struct lc_hash _ascon_128a_avx512 = {
+	.init = ascon_128a_init,
+	.update = ascon_absorb,
+	.final = ascon_squeeze,
+	.set_digestsize = NULL,
+	.get_digestsize = ascon_digestsize,
+	.sponge_permutation = ascon_avx512_permutation,
+	.sponge_add_bytes = ascon_c_add_bytes,
+	.sponge_extract_bytes = ascon_c_extract_bytes,
+	.sponge_newstate = ascon_c_newstate,
+	.sponge_rate = 128 / 8,
+	.statesize = sizeof(struct lc_ascon_hash),
+};
+LC_INTERFACE_SYMBOL(const struct lc_hash *, lc_ascon_128a_avx512) = &_ascon_128a_avx512;
+
+static const struct lc_hash _ascon_xof_avx512 = {
+	.init = ascon_xof_init,
+	.update = ascon_absorb,
+	.final = ascon_squeeze,
+	.set_digestsize = ascon_xof_set_digestsize,
+	.get_digestsize = ascon_xof_get_digestsize,
+	.sponge_permutation = ascon_avx512_permutation,
+	.sponge_add_bytes = ascon_c_add_bytes,
+	.sponge_extract_bytes = ascon_c_extract_bytes,
+	.sponge_newstate = ascon_c_newstate,
+	.sponge_rate = 64 / 8,
+	.statesize = sizeof(struct lc_ascon_hash),
+};
+LC_INTERFACE_SYMBOL(const struct lc_hash *, lc_ascon_xof_avx512) = &_ascon_xof_avx512;
+
+
+static const struct lc_hash _ascon_xofa_avx512 = {
+	.init = ascon_xofa_init,
+	.update = ascon_absorb,
+	.final = ascon_squeeze,
+	.set_digestsize = ascon_xof_set_digestsize,
+	.get_digestsize = ascon_xof_get_digestsize,
+	.sponge_permutation = ascon_avx512_permutation,
+	.sponge_add_bytes = ascon_c_add_bytes,
+	.sponge_extract_bytes = ascon_c_extract_bytes,
+	.sponge_newstate = ascon_c_newstate,
+	.sponge_rate = 64 / 8,
+	.statesize = sizeof(struct lc_ascon_hash),
+};
+LC_INTERFACE_SYMBOL(const struct lc_hash *, lc_ascon_xofa_avx512) = &_ascon_xofa_avx512;
---
+++ hash/src/asm/ascon_avx512/round.h
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2024, Stephan Mueller <smueller@chronox.de>
+ *
+ * License: see LICENSE file in root directory
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, ALL OF
+ * WHICH ARE HEREBY DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+ * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF NOT ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ *
+ * This code is derived from the file
+ * https://github.com/ascon/ascon-c/crypto_aead/ascon128v12/avx512/round.h
+ * which is subject to the following license:
+ *
+ * CC0 1.0 Universal
+ */
+
+#ifndef ROUND_H_
+#define ROUND_H_
+
+#include "ascon_hash.h"
+#include "ext_headers_x86.h"
+
+static inline void ascon_permutation_one_avx512(__m512i *z, long long C)
+{
+	long long x = 0;
+
+	__mmask8 mxor1 = 0x15;
+	__mmask8 mxor2 = 0x0b;
+	__m512i pxor1 = _mm512_set_epi64(x, x, x, 3, x, 1, x, 4);
+	__m512i pxor2 = _mm512_set_epi64(x, x, x, x, 2, x, 0, 4);
+	__m512i c = _mm512_set_epi64(x, x, x, 0, 0, C, 0, 0);
+	__m512i n = _mm512_set_epi64(x, x, x, 0, 0, ~0ll, 0, 0);
+	__m512i pchi1 = _mm512_set_epi64(x, x, x, 0, 4, 3, 2, 1);
+	__m512i pchi2 = _mm512_set_epi64(x, x, x, 1, 0, 4, 3, 2);
+	__m512i rot1 = _mm512_set_epi64(x, x, x, 7, 10, 1, 61, 19);
+	__m512i rot2 = _mm512_set_epi64(x, x, x, 41, 17, 6, 39, 28);
+	__m512i t0, t1, t2;
+	/* round constant + s-box layer */
+	t0 = _mm512_maskz_permutexvar_epi64(mxor1, pxor1, *z);
+	t0 = _mm512_ternarylogic_epi64(*z, t0, c, 0x96);
+	/* keccak s-box start */
+	t1 = _mm512_permutexvar_epi64(pchi1, t0);
+	t2 = _mm512_permutexvar_epi64(pchi2, t0);
+	t0 = _mm512_ternarylogic_epi64(t0, t1, t2, 0xd2);
+	/* keccak s-box end */
+	t1 = _mm512_maskz_permutexvar_epi64(mxor2, pxor2, t0);
+	t0 = _mm512_ternarylogic_epi64(t0, t1, n, 0x96);
+	/* linear layer */
+	t1 = _mm512_rorv_epi64(t0, rot1);
+	t2 = _mm512_rorv_epi64(t0, rot2);
+	*z = _mm512_ternarylogic_epi64(t0, t1, t2, 0x96);
+}
+
+#endif /* ROUND_H_ */
