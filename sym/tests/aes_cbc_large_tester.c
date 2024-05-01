413dc06f9fbdfc46b76a9ce5ae990d32ee96c07e
Stephan Mueller <smueller@chronox.de>
2024-05-01 13:27:21 +0200

    use common memory allocator for tests
    Signed-off-by: Stephan Mueller <smueller@chronox.de>

--- sym/tests/aes_cbc_large_tester.c
+++ sym/tests/aes_cbc_large_tester.c
@@ -22,8 +22,8 @@
 #include "ext_headers.h"
 #include "lc_aes.h"
 #include "ret_checkers.h"
+#include "test_helper.h"
 
-#define LC_AESNI_SIZE (1UL << 30)
 static int aes_cbc_large_aesni(void)
 {
 	LC_SYM_CTX_ON_STACK(aes_cbc, lc_aes_cbc);
@@ -32,22 +32,21 @@ static int aes_cbc_large_aesni(void)
 			  0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f };
 	uint8_t iv[] = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
 			 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f };
+	size_t len;
 	int ret;
 
-	pt = calloc(1, LC_AESNI_SIZE);
-	if (!pt)
-		return 77;
+	CKINT(test_mem(&pt, &len));
 
 	lc_sym_init(aes_cbc);
 	CKINT(lc_sym_setkey(aes_cbc, key, sizeof(key)));
 	CKINT(lc_sym_setiv(aes_cbc, iv, sizeof(iv)));
-	lc_sym_encrypt(aes_cbc, pt, pt, LC_AESNI_SIZE);
+	lc_sym_encrypt(aes_cbc, pt, pt, len);
 	lc_sym_zero(aes_cbc);
 
 	lc_sym_init(aes_cbc);
 	CKINT(lc_sym_setkey(aes_cbc, key, sizeof(key)));
 	CKINT(lc_sym_setiv(aes_cbc, iv, sizeof(iv)));
-	lc_sym_decrypt(aes_cbc, pt, pt, LC_AESNI_SIZE);
+	lc_sym_decrypt(aes_cbc, pt, pt, len);
 	lc_sym_zero(aes_cbc);
 
 out:
