68f91711e00c77156da571c7a69cc19602e7dd54
Stephan Mueller <smueller@chronox.de>
2024-07-30 07:55:17 +0200

    fix RUST wrappers
    Signed-off-by: Stephan Mueller <smueller@chronox.de>

--- rust/tests/lc_dilithium.rs
+++ rust/tests/lc_dilithium.rs
@@ -11,42 +11,41 @@ use std::mem::MaybeUninit;
 #[test]
 fn lc_rust_dilithium_ed25519() {
 	unsafe {
-		let mut sk: MaybeUninit<lc_dilithium_ed25519_sk> =
+		let mut sk: MaybeUninit<lc_dilithium_87_ed25519_sk> =
 			MaybeUninit::uninit();
-		let mut pk: MaybeUninit<lc_dilithium_ed25519_pk> =
+		let mut pk: MaybeUninit<lc_dilithium_87_ed25519_pk> =
 			MaybeUninit::uninit();
-		let mut sig: MaybeUninit<lc_dilithium_ed25519_sig> =
+		let mut sig: MaybeUninit<lc_dilithium_87_ed25519_sig> =
 			MaybeUninit::uninit();
 
 		let msg: [u8; 3] = [0x00, 0x01, 0x02];
 		let msg2: [u8; 3] = [0x00, 0x01, 0x02];
 
-		if lc_dilithium_ed25519_keypair(pk.as_mut_ptr(),
-						sk.as_mut_ptr(),
-						lc_seeded_rng) != 0 {
+		if lc_dilithium_87_ed25519_keypair(pk.as_mut_ptr(),
+						   sk.as_mut_ptr(),
+						   lc_seeded_rng) != 0 {
 			println!("Keypair generation failed");
 		}
 		pk.assume_init();
 		sk.assume_init();
 
-		if lc_dilithium_ed25519_sign(sig.as_mut_ptr(), msg.as_ptr(),
-					     msg.len(), sk.as_ptr(),
-					     lc_seeded_rng) != 0 {
+		if lc_dilithium_87_ed25519_sign(sig.as_mut_ptr(), msg.as_ptr(),
+						msg.len(), sk.as_ptr(),
+						lc_seeded_rng) != 0 {
 			println!("Signature generation failed");
 		}
 		sig.assume_init();
 
-		if lc_dilithium_ed25519_verify(sig.as_ptr(),
-					       msg.as_ptr(), msg.len(),
-					       pk.as_ptr()) != 0 {
+		if lc_dilithium_87_ed25519_verify(sig.as_ptr(), msg.as_ptr(),
+						  msg.len(), pk.as_ptr()) != 0 {
 			println!("Signature verification failed");
 		}
 
 		/* modify msg */
-		let ret = lc_dilithium_ed25519_verify(sig.as_ptr(),
-						      msg2.as_ptr(),
-						      msg2.len(),
-						      pk.as_ptr());
+		let ret = lc_dilithium_87_ed25519_verify(sig.as_ptr(),
+							 msg2.as_ptr(),
+							 msg2.len(),
+							 pk.as_ptr());
 		assert_ne!(-i32::from(EBADMSG as i16), ret);
 	}
 }
--- rust/tests/lc_kyber_kex.rs
+++ rust/tests/lc_kyber_kex.rs
@@ -11,30 +11,30 @@ use std::mem::MaybeUninit;
 #[test]
 fn lc_rust_kyber_x25519_kex() {
 	unsafe {
-		let mut pk_r: MaybeUninit<lc_kyber_x25519_pk> =
+		let mut pk_r: MaybeUninit<lc_kyber_1024_x25519_pk> =
 			MaybeUninit::uninit();
-		let mut sk_r: MaybeUninit<lc_kyber_x25519_sk> =
-			MaybeUninit::uninit();
-
-		let mut pk_i: MaybeUninit<lc_kyber_x25519_pk> =
-			MaybeUninit::uninit();
-		let mut sk_i: MaybeUninit<lc_kyber_x25519_sk> =
+		let mut sk_r: MaybeUninit<lc_kyber_1024_x25519_sk> =
 			MaybeUninit::uninit();
 
-		let mut pk_e_i: MaybeUninit<lc_kyber_x25519_pk> =
+		let mut pk_i: MaybeUninit<lc_kyber_1024_x25519_pk> =
 			MaybeUninit::uninit();
-		let mut ct_e_r: MaybeUninit<lc_kyber_x25519_ct> =
-			MaybeUninit::uninit();
-		let mut ct_e_i: MaybeUninit<lc_kyber_x25519_ct> =
-			MaybeUninit::uninit();
-		let mut ct_e_r_1: MaybeUninit<lc_kyber_x25519_ct> =
-			MaybeUninit::uninit();
-		let mut ct_e_r_2: MaybeUninit<lc_kyber_x25519_ct> =
-			MaybeUninit::uninit();
-		let mut sk_e: MaybeUninit<lc_kyber_x25519_sk> =
+		let mut sk_i: MaybeUninit<lc_kyber_1024_x25519_sk> =
 			MaybeUninit::uninit();
 
-		let mut tk: MaybeUninit<lc_kyber_x25519_ss> =
+		let mut pk_e_i: MaybeUninit<lc_kyber_1024_x25519_pk> =
+			MaybeUninit::uninit();
+		let mut ct_e_r: MaybeUninit<lc_kyber_1024_x25519_ct> =
+			MaybeUninit::uninit();
+		let mut ct_e_i: MaybeUninit<lc_kyber_1024_x25519_ct> =
+			MaybeUninit::uninit();
+		let mut ct_e_r_1: MaybeUninit<lc_kyber_1024_x25519_ct> =
+			MaybeUninit::uninit();
+		let mut ct_e_r_2: MaybeUninit<lc_kyber_1024_x25519_ct> =
+			MaybeUninit::uninit();
+		let mut sk_e: MaybeUninit<lc_kyber_1024_x25519_sk> =
+			MaybeUninit::uninit();
+
+		let mut tk: MaybeUninit<lc_kyber_1024_x25519_ss> =
 			MaybeUninit::uninit();
 
 		let mut ss_r: [u8; LC_KYBER_SSBYTES as usize] =
@@ -45,16 +45,18 @@ fn lc_rust_kyber_x25519_kex() {
 			[0; LC_KYBER_SSBYTES as usize];
 
 		// Generate static key for Bob
-		if lc_kyber_x25519_keypair(pk_r.as_mut_ptr(), sk_r.as_mut_ptr(),
-					   lc_seeded_rng) != 0 {
+		if lc_kyber_1024_x25519_keypair(pk_r.as_mut_ptr(),
+						sk_r.as_mut_ptr(),
+						lc_seeded_rng) != 0 {
 			println!("Keypair generation failed");
 		}
 		pk_r.assume_init();
 		sk_r.assume_init();
 
 		// Generate static key for Alice
-		if lc_kyber_x25519_keypair(pk_i.as_mut_ptr(), sk_i.as_mut_ptr(),
-					   lc_seeded_rng) != 0 {
+		if lc_kyber_1024_x25519_keypair(pk_i.as_mut_ptr(),
+						sk_i.as_mut_ptr(),
+						lc_seeded_rng) != 0 {
 			println!("Keypair generation failed");
 		}
 		pk_i.assume_init();
@@ -63,11 +65,11 @@ fn lc_rust_kyber_x25519_kex() {
 		// Perform unilaterally authenticated key exchange
 
 		// Run by Bob
-		if lc_kex_x25519_uake_initiator_init(pk_e_i.as_mut_ptr(),
-						     ct_e_i.as_mut_ptr(),
-						     tk.as_mut_ptr(),
-						     sk_e.as_mut_ptr(),
-						     pk_r.as_ptr()) != 0 {
+		if lc_kex_1024_x25519_uake_initiator_init(pk_e_i.as_mut_ptr(),
+							  ct_e_i.as_mut_ptr(),
+							  tk.as_mut_ptr(),
+							  sk_e.as_mut_ptr(),
+							  pk_r.as_ptr()) != 0 {
 			println!("Bob UAKE init");
 		}
 		pk_e_i.assume_init();
@@ -76,25 +78,20 @@ fn lc_rust_kyber_x25519_kex() {
 		sk_e.assume_init();
 
 		// Run by Alice
-		if lc_kex_x25519_uake_responder_ss(ct_e_r.as_mut_ptr(),
-						   ss_r.as_mut_ptr(),
-						   LC_KYBER_SSBYTES as usize,
-						   ptr::null(), 0,
-						   pk_e_i.as_ptr(),
-						   ct_e_i.as_ptr(),
-						   sk_r.as_ptr()) != 0 {
+		if lc_kex_1024_x25519_uake_responder_ss(
+			ct_e_r.as_mut_ptr(), ss_r.as_mut_ptr(),
+			LC_KYBER_SSBYTES as usize, ptr::null(), 0,
+			pk_e_i.as_ptr(), ct_e_i.as_ptr(), sk_r.as_ptr()) != 0 {
 			println!("Alice UAKE SS");
 		}
 		ct_e_r.assume_init();
 		sk_r.assume_init();
 
 		// Run by Bob
-		if lc_kex_x25519_uake_initiator_ss(ss_i.as_mut_ptr(),
-						   LC_KYBER_SSBYTES as usize,
-						   ptr::null(), 0,
-						   ct_e_r.as_ptr(),
-						   tk.as_ptr(),
-						   sk_e.as_ptr()) != 0 {
+		if lc_kex_1024_x25519_uake_initiator_ss(
+			ss_i.as_mut_ptr(), LC_KYBER_SSBYTES as usize,
+			ptr::null(), 0, ct_e_r.as_ptr(), tk.as_ptr(),
+			sk_e.as_ptr()) != 0 {
 			println!("Bob UAKE SS");
 		}
 
@@ -104,38 +101,29 @@ fn lc_rust_kyber_x25519_kex() {
 		// Perform mutually authenticated key exchange
 
 		// Run by Bob
-		if lc_kex_x25519_ake_initiator_init(pk_e_i.as_mut_ptr(),
-						    ct_e_i.as_mut_ptr(),
-						    tk.as_mut_ptr(),
-						    sk_e.as_mut_ptr(),
-						    pk_r.as_ptr()) != 0 {
+		if lc_kex_1024_x25519_ake_initiator_init(
+			pk_e_i.as_mut_ptr(), ct_e_i.as_mut_ptr(),
+			tk.as_mut_ptr(), sk_e.as_mut_ptr(),
+			pk_r.as_ptr()) != 0 {
 			println!("Bob AKE init");
 		}
 
 		// Run by Alice
-		if lc_kex_x25519_ake_responder_ss(ct_e_r_1.as_mut_ptr(),
-						  ct_e_r_2.as_mut_ptr(),
-						  ss_r.as_mut_ptr(),
-						  LC_KYBER_SSBYTES as usize,
-						  ptr::null(), 0,
-						  pk_e_i.as_ptr(),
-						  ct_e_i.as_ptr(),
-						  sk_r.as_ptr(),
-						  pk_i.as_ptr()) != 0 {
+		if lc_kex_1024_x25519_ake_responder_ss(
+			ct_e_r_1.as_mut_ptr(), ct_e_r_2.as_mut_ptr(),
+			ss_r.as_mut_ptr(), LC_KYBER_SSBYTES as usize,
+			ptr::null(), 0, pk_e_i.as_ptr(), ct_e_i.as_ptr(),
+			sk_r.as_ptr(), pk_i.as_ptr()) != 0 {
 			println!("Alice AKE SS");
 		}
 		ct_e_r_1.assume_init();
 		ct_e_r_2.assume_init();
 
 		// Run by Bob
-		if lc_kex_x25519_ake_initiator_ss(ss_i.as_mut_ptr(),
-						  LC_KYBER_SSBYTES as usize,
-						  ptr::null(), 0,
-						  ct_e_r_1.as_ptr(),
-						  ct_e_r_2.as_ptr(),
-						  tk.as_ptr(),
-						  sk_e.as_ptr(),
-						  sk_i.as_ptr()) != 0 {
+		if lc_kex_1024_x25519_ake_initiator_ss(
+			ss_i.as_mut_ptr(), LC_KYBER_SSBYTES as usize,
+			ptr::null(), 0, ct_e_r_1.as_ptr(), ct_e_r_2.as_ptr(),
+			tk.as_ptr(), sk_e.as_ptr(), sk_i.as_ptr()) != 0 {
 			println!("Bob AKE SS");
 		}
 
