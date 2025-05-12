c5e3d433d173c2ebdd45ca07707b01fa262a775d
Quin-Darcy <pohmsuindraguli@gmail.com>
2025-05-12 09:28:10 -0500

    fix: resolve function declaration warning by updating imports
    Update all modules to import FFI bindings from the `ffi` module instead
    of including bindings directly.

--- rust/src/lcr_hash.rs
+++ rust/src/lcr_hash.rs
@@ -17,19 +17,8 @@
  * DAMAGE.
  */
 
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-
-//Disable warnings about problematic FFI types
-#[allow(improper_ctypes)]
-//Disable warnings about unused symbols from leancrypto
-#[allow(dead_code)]
-mod leancrypto {
-	include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
-}
-
 use std::ptr;
+use crate::ffi::leancrypto;
 use crate::error::HashError;
 
 pub enum lcr_hash_type {
