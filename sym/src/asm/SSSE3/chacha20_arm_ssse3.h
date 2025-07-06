34e327da9af592635bcf4c9e3308123b0b3a34e2
Stephan Mueller <smueller@chronox.de>
2025-07-06 21:34:32 +0200

    ChaCha20: fix execution on Windows
    Signed-off-by: Stephan Mueller <smueller@chronox.de>

--- sym/src/asm/SSSE3/chacha20_arm_ssse3.h
+++ sym/src/asm/SSSE3/chacha20_arm_ssse3.h
@@ -37,8 +37,8 @@ extern "C" {
  * @param [in] counter counter / nonce material collected into 4 32-bit elements
  *		       array in host byte order
  */
-void ChaCha20_ssse3(uint8_t *out, const uint8_t *in, size_t len,
-		    const uint32_t key[8], const uint32_t counter[4]);
+void SYSV_ABI ChaCha20_ssse3(uint8_t *out, const uint8_t *in, size_t len,
+			     const uint32_t key[8], const uint32_t counter[4]);
 
 #ifdef __cplusplus
 }
