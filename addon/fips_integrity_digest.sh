b8da8b56c39a3e54f12c63084369604699ebcac3
Stephan Mueller <smueller@chronox.de>
2025-09-15 17:31:23 +0200

    FIPS: add helper tool to extract library digest
    Signed-off-by: Stephan Mueller <smueller@chronox.de>

---
+++ addon/fips_integrity_digest.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#
+# Tool to extract the message digest of the FIPS library. This digest provides
+# an unambiguous reference that can be added to a FIPS Security Policy
+# to refer to the library version.
+#
+# Invocation:
+#	$0 <libleancrypto-fips.so>
+#
+# The returned data is the message digest that is applied by the integrity
+# check, typically a SHA3-256 digest.
+#
+
+SOFILE="$1"
+
+FIPSDATASEGMENT=".lc_fips_integrity_data"
+SECOUTFILE="extracted_sections.digest"
+
+OBJCOPY="objcopy"
+XXD="xxd"
+
+################################################################################
+
+if [ -z "$SOFILE" -o ! -f "$SOFILE" ]
+then
+	echo "ERROR: shared library file $SOFILE not found"
+	exit 1
+fi
+
+trap "rm -f $FIPSDATASEGMENT.$$" 0 1 2 15
+
+# Extract section with digest
+$OBJCOPY							\
+ --dump-section $FIPSDATASEGMENT=$FIPSDATASEGMENT.$$		\
+ $SOFILE
+if [ $? -ne 0 ]
+then
+	echo "ERROR: $OBJCOPY command failed: $?"
+	exit $?
+fi
+
+# Print out the hex string
+$XXD -l 16 -p $FIPSDATASEGMENT.$$ | sed -e 's/../:&/2g'
