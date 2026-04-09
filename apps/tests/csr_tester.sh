5bea593fb36020df5ccc8fcc302b4bc799654e1f
Stephan Mueller <smueller@chronox.de>
2026-04-09 08:04:13 +0200

    tests: make csr_tester.sh and siggen_tester.sh consistent
    Signed-off-by: Stephan Mueller <smueller@chronox.de>

--- apps/tests/csr_tester.sh
+++ apps/tests/csr_tester.sh
@@ -164,17 +164,13 @@ check_one() {
 
 	# TODO: it is totally bizare why sometimes OpenSSL says it cannot
 	# find a certificate
-	# we may have composite algos, ignore
-#	if [ "x$inform" != "xPEM" ]
+#	echo "=== Checking file $inputfile with OpenSSL ==="
+#	$OPENSSL x509 -in $inputfile -inform $inform -text -noout
+#	if [ $? -ne 0 ]
 #	then
-#		echo "=== Checking file $inputfile with OpenSSL ==="
-#		$OPENSSL x509 -in $inputfile -inform $inform -text -noout
-#		if [ $? -ne 0 ]
-#		then
-#			echo_fail "Parsing of file $inputfile was unsuccessful"
-#		else
-#			echo_success "Parsing of file $inputfile was successful"
-#		fi
+#		echo_fail "Parsing of file $inputfile was unsuccessful"
+#	else
+#		echo_success "Parsing of file $inputfile was successful"
 #	fi
 
 	if [ "x$inform" = "xPEM" ]
