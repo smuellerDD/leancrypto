48f4d5fa978855bc43b4e80a1e3874d879025b03
Stephan Mueller <smueller@chronox.de>
2026-05-21 08:14:53 +0200

    rustls: rearrange precedence order in KEX
    Signed-off-by: Stephan Mueller <smueller@chronox.de>

--- rustls/src/kx_group/mod.rs
+++ rustls/src/kx_group/mod.rs
@@ -6,24 +6,24 @@ mod x25519;
 //#[cfg(not(feature = "fips"))]
 pub use x25519::X25519;
 
-mod kem;
-pub use kem::{MLKEM768, MLKEM1024};
+mod mlkem;
+pub use mlkem::{MLKEM768, MLKEM1024};
 
-mod hybrid_kem;
-pub use hybrid_kem::{X25519MLKEM768};
+mod x25519mlkem768;
+pub use x25519mlkem768::{X25519MLKEM768};
 
 /// Key exchanges enabled by default by this provider:
 /// * [X25519MLKEM768]
-/// * [X25519] (if fips feature not enabled)
-/// * [MLKEM768]
 /// * [MLKEM1024]
+/// * [MLKEM768]
+/// * [X25519] (if fips feature not enabled)
 ///
 /// If the `prefer-post-quantum` feature is enabled, X25519MLKEM768 will
 /// be the first group offered, otherwise it will be the last.
 pub static DEFAULT_KX_GROUPS: &[&dyn SupportedKxGroup] = &[
 	X25519MLKEM768,
+	MLKEM1024,
+	MLKEM768,
 	//#[cfg(not(feature = "fips"))]
 	X25519,
-	MLKEM768,
-	MLKEM1024
 ];
