c1bd9904127bfb9ba15f2a96bc7f1afc574fa6a6
Stephan Mueller <smueller@chronox.de>
2026-05-22 08:25:06 +0200

    OpenSSL test: make it less chatty
    Signed-off-by: Stephan Mueller <smueller@chronox.de>

--- asn1/tests/testcerts/lc_openssl_server.sh
+++ asn1/tests/testcerts/lc_openssl_server.sh
@@ -11,12 +11,13 @@ fi
 openssl							\
   s_server						\
   -naccept 1						\
+  -quiet						\
   -port $PORT						\
   -cert ${DIR}ed25519_leaf.der				\
   -certform DER						\
   -key ${DIR}ed25519_leaf.privkey			\
   -keyform DER						\
-  -state -www						\
+  -www							\
   -no_cache -tls1_3					\
   -ciphersuites TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_128_GCM_SHA256 \
   -groups X25519:MLKEM768:MLKEM1024:X25519MLKEM768
