e8a05156228c51a2be30a690e760c1844121dc6a
Stephan Mueller <smueller@chronox.de>
2026-08-02 10:43:09 +0200

    NetBSD: disble mmap use
    Signed-off-by: Stephan Mueller <smueller@chronox.de>

--- apps/src/lc_x509_generator_file_helper.c
+++ apps/src/lc_x509_generator_file_helper.c
@@ -138,7 +138,7 @@ void release_data_memory(uint8_t *memory, size_t memory_length,
 }
 
 #if (defined(__CYGWIN__) || defined(_WIN32) || defined(__sun) ||               \
-     defined(__OpenBSD__))
+     defined(__OpenBSD__) || defined(__NetBSD))
 
 int get_data(const char *filename, uint8_t **memory, size_t *memory_length,
 	     enum lc_pem_flags pem_flags)
